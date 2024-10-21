8dc3edc0dc45f17b0d263a01e6a95bc808b02ea7
Anjali-Bhagwate <bhagwateanjali2810@gmail.com>
2024-10-21 22:42:33 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,6 @@
+function a(){
+    var email = document.getElementById("inputEmail4").value;
+    var firstNmae = document.getElementById("inputFirstName").value;
+    alert(email);
+    document.getElementById("id-name").innerHTML= firstNmae;
+}
